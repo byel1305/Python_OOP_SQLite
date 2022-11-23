ade2bc7802dad1af0496e33882cfbcb968df722b
Gabryel Júnio Costa Teixeira <90285723+byel1305@users.noreply.github.com>
2022-11-23 23:58:06 +0000

    Fizemos um exemplo simples de orientação a objetos

---
+++ main.py
@@ -0,0 +1,10 @@
+#Importa nosso arquivo Pessoa.py no diretório model
+from model.Pessoa import Pessoa
+
+#Exemplo de uso
+poyatos=Pessoa(1,"Gabryel Junio")
+print(poyatos)
+
+#Quero mostrsr só o nome
+print(poyatos.nome)
+
